75af1df6d94127ec504190db180a35f4997f92ff
Aliiiw <1aliirahimi1@gmail.com>
2022-05-18 13:56:15 +0430

    Add the AppBar and direct icon
    adding the instagram name and appbar and direct  icon and number of notifications hard code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,47 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Instagram',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'bilong',
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                const Icon(
+                  Icons.near_me_outlined,
+                  color: Colors.black,
+                  size: 35,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, left: 20),
+                  child: const CircleAvatar(
+                    radius: 8,
+                    backgroundColor: Colors.redAccent,
+                    child: Text(
+                      '7',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      body: const Center(
         child: Text('Home'),
       ),
     );
