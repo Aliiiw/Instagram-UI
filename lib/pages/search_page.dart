8ed70d6d152f19ab05ff7008af49fca345a74449
Ali Rahimi <1aliirahimi1@gmail.com>
2022-05-19 18:06:34 +0430

    Complete the search page
    completing search page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -5,9 +5,278 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Search'),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 4),
+                      prefixIcon: Icon(Icons.search, color: Colors.grey[800]),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.grey[800],
+                      ),
+                      label: const Text('Search'),
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                      border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                          )),
+                      focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.grey,
+                            style: BorderStyle.solid
+                      ))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 2),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp1.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp2.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp3.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp4.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp5.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp6.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp7.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp8.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp9.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp10.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp6.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp5.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp9.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp3.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp6.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp10.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp4.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/sp6.jpeg'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
